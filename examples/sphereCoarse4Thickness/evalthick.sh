36560d9399c51791be7551c69c88272bda799a6f
Cesare Corrado <cesare.corrado2009@gmail.com>
2015-08-28 16:22:10 +0100

    some  modif

--- examples/sphereCoarse4Thickness/evalthick.sh
+++ examples/sphereCoarse4Thickness/evalthick.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-EXEDIR='/home/cc14/Codes/thicktools'
-DIRM3D='/home/cc14/Codes/meshtools3d'
+EXEDIR='../../thicknessOld'
+DIRM3D='../../'
+DATAFILE='data_sphereCoarse'
+M3DEXE=${DIRM3D}/meshtools3d
 EXE=${EXEDIR}'/thicknessOld'
 DIR=${DIRM3D}'/examples/sphereCoarse4PotentialOUT'
 MBNAME=${DIR}/sphere
@@ -8,5 +10,8 @@ LENDO=${MBNAME}_endo.vtx
 LEPI=${MBNAME}_epi.vtx
 GRAD=${MBNAME}.grad
 FLAG=0
+
+${M3DEXE} -f ${DATAFILE}
+
 
 ${EXE} ${MBNAME} ${LENDO} ${GRAD} ${LEPI} $FLAG
